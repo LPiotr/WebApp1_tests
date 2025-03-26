8eb2f2f3a9d72b886705e1342dd91829ff031062
LPiotr <piotrl1@live.com>
2025-03-26 23:19:01 +0100

    Dodaj pliki projektów.

---
+++ Osoba.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApp1
+{
+    public class Osoba
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public Osoba(int id, string imie, string nazwisko)
+        {
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+    }
+ 
+    public class OsobaService
+    {
+        public List<Osoba> GetOsoby()
+        {
+            return new List<Osoba>
+            {
+                new Osoba(1, "Jan", "Kowalski"),
+                new Osoba(2, "Anna", "Nowak"),
+                new Osoba(3, "Piotr", "Wiśniewski"),
+                new Osoba(4, "Maria", "Dąbrowska"),
+            };
+        }
+    }
+}
---
+++ Pages/Osoba.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp1;
+using System.Collections.Generic;
+
+namespace WebApp1.Pages
+{
+    public class OsobaModel : PageModel
+    {
+        public List<Osoba> Osoby { get; set; }
+
+        public void OnGet()
+        {
+            var service = new OsobaService();
+            Osoby = service.GetOsoby();
+        }
+    }
+}
